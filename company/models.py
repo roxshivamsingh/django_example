29db9e352e3b2a7cfd5b2dba020d037b5ad68c2a
Shivam Singh <roxshivamsingh@gmail.com>
2025-06-24 22:41:58 +0530

    ADDED: App > Company - modal fields;

--- company/models.py
+++ company/models.py
@@ -14,3 +14,5 @@ class Company(models.Model):
     company_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
